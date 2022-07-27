900ec15e6f64fd16c3fa6aae816b7cc94e1889bf
maniac <alireza.salehi@gmail.com>
2022-07-27 17:09:44 +0430

    fix namespace problem

--- app/Http/Controllers/EntityController.php
+++ app/Http/Controllers/EntityController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Entity;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Request;
 
 class EntityController extends Controller
 {
