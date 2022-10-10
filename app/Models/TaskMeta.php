d04af5f05f9f629db525e21e0e7206768e3dba65
maniac <alireza.salehi@gmail.com>
2022-10-10 10:46:02 +0330

    add soft deletes to task meta

--- app/Models/TaskMeta.php
+++ app/Models/TaskMeta.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TaskMeta extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $primaryKey = 'task_meta_id';
     protected $fillable = ['task_key', 'task_value', 'task_ref_id', 'column_ref_id'];
